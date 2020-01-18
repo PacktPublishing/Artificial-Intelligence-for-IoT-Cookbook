ce6a20a3be1ce8cd63d3a1566987f4f3a075fc89
microshak <roshak@hotmail.com>
2020-01-17 19:10:21 -0800

    ch4

---
+++ Ch4/Flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import pickle
+import xgboost as xgb
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST']) 
+def predict():
+    model_filename = os.path.join(os.getcwd(), 'bst.sav')
+    loaded_model = pickle.load(open(model_filename, "rb"))
+    x_test = pd.DataFrame(request.json)
+    y_pred = loaded_model.predict(xgb.DMatrix(x_test))
+    y_pred[y_pred > 0.5] = 1
+    y_pred[y_pred <= 0.5] = 0
+    return int(y_pred[0])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
