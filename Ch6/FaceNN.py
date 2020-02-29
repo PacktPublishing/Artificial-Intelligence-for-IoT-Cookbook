1427955e7b1ed8382766fe3ed7262a7d0babb32e
Microshak <v-roshmi@microsoft.com>
2020-02-28 18:34:05 -0800

    mroe

--- Ch6/FaceNN.py
+++ Ch6/FaceNN.py
@@ -15,24 +15,14 @@ def FaceNN(frame):
     net.setInput(blob)
     detections = net.forward()
 
-    # loop over the detections
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
         confidence = detections[0, 0, i, 2]
-
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if confidence < .8:
+     if confidence < .8:
             continue
 
-        # compute the (x, y)-coordinates of the bounding box for the
-        # object
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
 
-        # draw the bounding box of the face along with the associated
-        # probability
         text = "{:.2f}%".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(frame, (startX, startY), (endX, endY),
@@ -49,6 +39,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
