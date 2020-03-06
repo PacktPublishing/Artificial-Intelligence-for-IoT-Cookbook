0a274c8ac040de9e5e05b898194c1cfd09214796
Microshak <v-roshmi@microsoft.com>
2020-03-06 14:48:43 -0800

    weekend

--- Ch6/pyImage/classify.py
+++ Ch6/pyImage/classify.py
@@ -1,41 +1,57 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
+from torch.utils.data.sampler import SubsetRandomSampler
+    
+
+datadir = './data/train'
+valid_size = .3
+epochs = 3
+steps = 0
+running_loss = 0
+print_every = 10
+train_losses, test_losses = [], []
+
+def print_score(torch, testloader, inputs, device, model,criterion,labels):
+    test_loss = 0
+    accuracy = 0
+    model.eval()
+    
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            logps = model.forward(inputs)
+            batch_loss = criterion(logps, labels)
+            test_loss += batch_loss.item()
+
+            ps = torch.exp(logps)
+            top_p, top_class = ps.topk(1, dim=1)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+    train_losses.append(running_loss/len(trainloader))
+    test_losses.append(test_loss/len(testloader))                    
+    print(f"Epoch {epoch+1}/{epochs} Train loss: {running_loss/print_every:.3f} Test loss: {test_loss/len(testloader):.3f} Test accuracy: {accuracy/len(testloader):.3f}")
+    
+    return test_loss, accuracy
 
 
-data_dir = './data/train'
+train_transforms = transforms.Compose([transforms.Resize(224),transforms.ToTensor()])
+test_transforms = transforms.Compose([transforms.Resize(224), transforms.ToTensor()])
+train_data = datasets.ImageFolder(datadir, transform=train_transforms)
+test_data = datasets.ImageFolder(datadir, transform=test_transforms)
+num_train = len(train_data)
+indices = list(range(num_train))
+split = int(np.floor(valid_size * num_train))
+np.random.shuffle(indices)
+train_idx, test_idx = indices[split:], indices[:split]
+train_sampler = SubsetRandomSampler(train_idx)
+test_sampler = SubsetRandomSampler(test_idx)
+trainloader = torch.utils.data.DataLoader(train_data, sampler=train_sampler, batch_size=1)
+testloader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=1)
 
-
-def load_split_train_test(datadir, valid_size = .2):
-    train_transforms = transforms.Compose([
-                                       transforms.Resize(224),
-                                       transforms.ToTensor(),
-                                        ])
-
-    test_transforms = transforms.Compose([transforms.Resize(224),
-                                      transforms.ToTensor(),
-                                      ])
-
-    train_data = datasets.ImageFolder(datadir, transform=train_transforms)
-    test_data = datasets.ImageFolder(datadir, transform=test_transforms)
-
-    num_train = len(train_data)
-    indices = list(range(num_train))
-    split = int(np.floor(valid_size * num_train))
-    np.random.shuffle(indices)
-    from torch.utils.data.sampler import SubsetRandomSampler
-    train_idx, test_idx = indices[split:], indices[:split]
-    train_sampler = SubsetRandomSampler(train_idx)
-    test_sampler = SubsetRandomSampler(test_idx)
-    trainloader = torch.utils.data.DataLoader(train_data, sampler=train_sampler, batch_size=1)
-    testloader = torch.utils.data.DataLoader(test_data, sampler=test_sampler, batch_size=1)
-    return trainloader, testloader
-
-trainloader, testloader = load_split_train_test(data_dir, .2)
 print(trainloader.dataset.classes)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -44,21 +60,12 @@ model = models.resnet50(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
     
-model.fc = nn.Sequential(nn.Linear(2048, 512),
-                                 nn.ReLU(),
-                                 nn.Dropout(0.2),
-                                 nn.Linear(512, 10),
-                                 nn.LogSoftmax(dim=1))
+model.fc = nn.Sequential(nn.Linear(2048, 512), nn.ReLU(), nn.Dropout(0.2), nn.Linear(512, 10), nn.LogSoftmax(dim=1))
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 model.to(device)
 
 
-epochs = 1
-steps = 0
-running_loss = 0
-print_every = 10
-train_losses, test_losses = [], []
 
 for epoch in range(epochs):
     for inputs, labels in trainloader:
@@ -72,31 +79,9 @@ for epoch in range(epochs):
         running_loss += loss.item()
         
         if steps % print_every == 0:
-            test_loss = 0
-            accuracy = 0
-            model.eval()
-            with torch.no_grad():
-                for inputs, labels in testloader:
-                    inputs, labels = inputs.to(device), labels.to(device)
-                    logps = model.forward(inputs)
-                    batch_loss = criterion(logps, labels)
-                    test_loss += batch_loss.item()
-                    
-                    ps = torch.exp(logps)
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = top_class == labels.view(*top_class.shape)
-                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-
-            train_losses.append(running_loss/len(trainloader))
-            test_losses.append(test_loss/len(testloader))                    
-            print(f"Epoch {epoch+1}/{epochs}.. "
-                  f"Train loss: {running_loss/print_every:.3f}.. "
-                  f"Test loss: {test_loss/len(testloader):.3f}.. "
-                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+            test_loss, accuracy = print_score(torch, testloader, inputs, device, model,criterion,labels)
             running_loss = 0
             model.train()
-torch.save(model, 'aerialmodel.pth')
-plt.plot(train_losses, label='Training loss')
-plt.plot(test_losses, label='Validation loss')
-plt.legend(frameon=False)
-plt.show()
+torch.save(model, 'saftey.pth')
+
+
--- Ch6/pyImage/inferance.py
+++ Ch6/pyImage/inferance.py
@@ -13,12 +13,11 @@ from torch.autograd import Variable
 
 test_transforms = transforms.Compose([transforms.Resize(224),
                                       transforms.ToTensor(),
-                                      #transforms.Normalize([0.485, 0.456, 0.406],
-                                      #                     [0.229, 0.224, 0.225])
+                                     
                                      ])
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model=torch.load('aerialmodel.pth')
+model=torch.load('safety.pth')
 model.eval()
 
 def predict_image(image):
