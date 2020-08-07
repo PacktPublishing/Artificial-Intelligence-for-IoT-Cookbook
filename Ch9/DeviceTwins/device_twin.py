e8c12098b7990ccb0b9113d4e2764b2260651dfc
microshak <roshak@hotmail.com>
2020-08-07 09:56:28 -0700

    twins

---
+++ Ch9/DeviceTwins/device_twin.py
@@ -0,0 +1,34 @@
+import asyncio
+from six.moves import input
+from azure.iot.device.aio import IoTHubDeviceClient
+
+
+async def main():
+    device_client = IoTHubDeviceClient.create_from_connection_string("HostName=University-IoT-Cart.azure-devices.net;DeviceId=Pi_Envirnoment;SharedAccessKey=TWnLYcXf/sxYoacZry0akx7knPOa2gSojrkZ7oyBfX0=")
+
+    await device_client.connect()
+
+    async def twin_patch_listener(device_client):
+        while True:
+            patch = await device_client.receive_twin_desired_properties_patch()  # blocking call
+            print("the data in the desired properties patch was: {}".format(patch))
+
+    def quit_listener():
+        while True:
+            selection = input("Press Q to quit\n")
+            if selection == "Q" or selection == "q":
+                print("Quitting...")
+                break
+
+    asyncio.create_task(twin_patch_listener(device_client))
+
+    loop = asyncio.get_running_loop()
+    user_finished = loop.run_in_executor(None, quit_listener)
+
+    await user_finished
+
+    await device_client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
