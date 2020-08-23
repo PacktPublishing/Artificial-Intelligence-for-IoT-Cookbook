41c4f6eee423fdb8bc74892b6a0e607fe414509b
Microshak <roshak@hotmail.com>
2020-08-23 14:46:12 -0700

    Android

---
+++ Ch9/AIandIoTCookbook/app/src/main/java/com/microshak/aiandiotcookbook/MainActivity.java
@@ -0,0 +1,27 @@
+package com.microshak.aiandiotcookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.tensorflow.lite.Interpreter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tflite = new Interpreter(loadModelFile(activity));
+        tflite.run(inputdata, labelProbArray);
+    }
+
+    private MappedByteBuffer loadModelFile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(getModelPath());
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+}
